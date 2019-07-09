e1a9c9f3688f827b74af287da5c263a03aff1d54
Chase Shields <cwshields2@gmail.com>
2019-07-09 16:37:06 -0500

    completed

--- stage-2/src/Components/StoreFront/StoreFront.js
+++ stage-2/src/Components/StoreFront/StoreFront.js
@@ -7,15 +7,15 @@ class StoreFront extends Component {
       return (
         <div className="product-container" key={index}>
           <h2>{element.title}</h2>
-          <img src={element.image} alt="" />
+          <img onClick={() => this.props.addToCart(element)} src={element.image} alt="" />
           <h2>{element.desc}</h2>
           <h3>{"$" + element.price + ".00"}</h3>
-          <button onClick={() => this.props.addToCart(element)}>Purchase!</button>
         </div>
       )
     })
     return (
       <div className="storefront-container">
+        {productDisplay}
       </div>
     )
   }
